764c2ea89b6f680bc43dc5370e98917b3504c8d6
vaishnavi754 <81965405+vaishnavipaliya11@users.noreply.github.com>
2022-02-15 04:36:59 -1200

    feat: added functionality to the slider

---
+++ products/range.js
@@ -0,0 +1,8 @@
+
+var rangeslider = document.getElementById("sliderRange");
+var output = document.getElementById("demo");
+output.innerHTML = rangeslider.value;
+  
+rangeslider.oninput = function() {
+  output.innerHTML = this.value;
+}
